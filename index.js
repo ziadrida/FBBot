8422176547104dbd1dba8b970dc22c22f9ceef42
unknown <ziad.i.rida@gmail.com>
2017-07-03 00:11:40 +0300

    update

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ const request = require('request')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send ('Hello - It is now working!')
+})
+
+
+app.get('/webhook/', function(req, res)){
+  if(req.query['hub.verify_token'] ===
+'my_voice_is_my_password_verify_me') {
+  res.send (req.query['hub.challenge'])
+}
+res.send('No entry')
+})
+
+app.listen(app.get('port'), function(){
+  console.log('running on port', app.get('port'))
+})
