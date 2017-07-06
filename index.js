337eeeca978ddcd9f3999ae9380cff46257789a2
unknown <ziad.i.rida@gmail.com>
2017-07-06 11:47:53 +0300

    update - added post

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
+// mongodb
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://heroku_lrtnbx3s:5c5t5gtstipg3k6b9n6721mfpn@ds149412.mlab.com:49412/heroku_lrtnbx3s';
+
 //add all of code
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -83,6 +89,13 @@ function receivedMessage(event) {
     // if message contains http, then it is a pricing request
     if (messageText.toUpperCase().indexOf ("HTTP") >= 0) {
       sendTextMessage(senderID, 'pricing now...');
+      // insertDocument into mongoDB
+      MongoClient.connect(url, function(err, db) {
+        assert.equal(null, err);
+        insertDocument(db, function() {
+          db.close();
+  });
+});
     }
     if ( messageText.toUpperCase().indexOf("PHONE") >= 0) {
       sendTextMessage(senderID, 'Our main phone number is 0785000010');
@@ -139,9 +152,40 @@ function callSendAPI(messageData) {
     }
   });
 }
-
 // end of code that i copied from quick start (Seema)
 
 app.listen(app.get('port'), function(){
   console.log('running on port', app.get('port'))
 })
+
+// insertDocument copied example fromhttps://docs.mongodb.com/getting-started/node/insert/
+var insertDocument = function(db, callback) {
+   db.collection('restaurants').insertOne( {
+      "address" : {
+         "street" : "2 Avenue",
+         "zipcode" : "10075",
+         "building" : "1480",
+         "coord" : [ -73.9557413, 40.7720266 ]
+      },
+      "borough" : "Manhattan",
+      "cuisine" : "Italian",
+      "grades" : [
+         {
+            "date" : new Date("2014-10-01T00:00:00Z"),
+            "grade" : "A",
+            "score" : 11
+         },
+         {
+            "date" : new Date("2014-01-16T00:00:00Z"),
+            "grade" : "B",
+            "score" : 17
+         }
+      ],
+      "name" : "Vella",
+      "restaurant_id" : "41704620"
+   }, function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
