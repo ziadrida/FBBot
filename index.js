f56681fed924a96311536740b26fa7e8d8f99ef1
unknown <ziad.i.rida@gmail.com>
2017-07-30 23:14:58 +0300

    update - added wit NLP

--- index.js
+++ index.js
@@ -393,7 +393,7 @@ if (message.nlp) {
     console.log("m>>>>>>>>> matchEntity response:",msg);
     sendTextMessage(senderID,msg);
   });
-  console.log("m>>>>>>>>> matchEntity response:",res);
+
 
   queryWit(message.text, N).then((entities)  => {
     console.log("** entities:",entities);
@@ -1102,7 +1102,7 @@ function matchEntity(entity_name,value,callback) {
 
 
           // Peform a simple find and return all the documents
-          collection.find({"entity_name" : entity_name, "value" : value }).limit(1).then(function(docs) {
+          collection.find({"entity_name" : entity_name, "value" : value }).limit(1).toArray().then(function(docs) {
 
             console.log("*** docs:", docs);
             assert.equal(null, err);
