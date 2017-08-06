42bde5b9e5cdbacd3ab81df2da805b74ec0454d6
unknown <ziad.i.rida@gmail.com>
2017-08-06 10:52:42 +0300

    update - added category and dbutil

--- mongoUtil.js
+++ mongoUtil.js
@@ -1,11 +1,13 @@
 var MongoClient = require( 'mongodb' ).MongoClient;
 
 var _db;
+var mongodbUrl = 'mongodb://heroku_lrtnbx3s:5c5t5gtstipg3k6b9n6721mfpn@ds149412.mlab.com:49412/heroku_lrtnbx3s';
 
 module.exports = {
 
-  connectToServer: function( callback ) {
-    MongoClient.connect( "mongodb://localhost:27017/marankings", function( err, db ) {
+  connectToDB: function( callback ) {
+    MongoClient.connect( mongodbUrl, function( err, db ) {
+      console.log("  DB CONNECTED")''
       _db = db;
       return callback( err );
     } );
