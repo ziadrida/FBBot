0e65805fdfe81f447203e463262fe938a985437a
unknown <ziad.i.rida@gmail.com>
2017-08-09 13:51:10 +0300

    update - added category and dbutil

--- index.js
+++ index.js
@@ -1416,7 +1416,8 @@ function getPricing(senderID,item) {
     for (i=0 ; i < cats.length && i<4 ; i++) {
       console.log("+++++++++++++= ",cats[i]);
         catList.push({
-          "title" : cats[i].category_name + "/ " + cats[i].score.toFixed(2),
+          "title" : cats[i].category_name + "/customs:"+cats[i].customs + "/tax amm,aq:"+cat[i].tax_amm+
+            ","+cat[i].tax_aqaba + " /"+ cats[i].score.toFixed(2),
           "subtitle"  : cats[i].category_name_ar,
           buttons : [{
             "title": "Select أختار",
