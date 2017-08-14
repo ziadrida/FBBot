4d66309f8877b6e543d542b5e21fa231cb09dd53
unknown <ziad.i.rida@gmail.com>
2017-08-14 12:11:46 +0300

    update - added http insert

--- index.js
+++ index.js
@@ -377,14 +377,21 @@ function handleEvent(senderID, event) {
    var detailsMsg_ar = pricingDetailMsg_ar.replace("<سعر>",payloadMsg.quotation.item.price);
   var pricing = {
     chargableWeight: payloadMsg.quotation.item.chargableWeight,
-    shipping: payloadMsg.quotation.item.shipping
+    shipping: payloadMsg.quotation.item.shipping,
+    shippingAtOriginMsg: (payloadMsg.quotation.item.shipping <0? 'does not include shipping at origin (if any)\n':
+            'includes shipping at origin of:' +pricing.shipping +'\n'),
+    price: payloadMsg.quotation.item.price,
+    category_name: payloadMsg.quotation.item.category_info.category_name,
+    amm_customs: (payloadMsg.quotation.item.category_info.amm_customs * 100).toFixed(1),
+    amm_tax: (payloadMsg.quotation.item.category_info.amm_tax * 100).toFixed(1),
+    aqaba_tax: (payloadMsg.quotation.item.category_info.aqaba_tax * 100).toFixed(1)
   }
    var detailsMsg_en =
-   `Chargable weight: ${pricing.chargableWeight}kg. Shipping weight may be higher than actual product weight\n`+
-   `Price at origin:$<price> ;includes shipping at origin of:$${pricing.shipping}\n`+
-   `Category: <category_name>\n`+
-   `Amman customs of <amm_customs>% and tax:<amm_tax>%\n` +
-   `Aqaba customs 0% and tax <aqaba_customs>%\n`+
+   `Chargable weight: ${pricing.chargableWeight} KG. Shipping weight may be higher than actual product weight\n`+
+   `Price at origin:$<price> ;${pricing.shippingAtOriginMsg}`+
+   `Category: ${pricing.category_name}\n`+
+   `Amman customs of ${pricing.amm_customs}% and tax:${pricing.amm_tax}%\n` +
+   `Aqaba customs 0% and tax ${pricing.aqaba_customs}%\n`+
    `Warranty is at source country (add ${warranty_price}% for local warranty)\n`+
    `Prices include the actual item price + all shipping + all taxes and expenses.\n` +
    `Our guarantee 1. best price 2. price will not change upon arrival 3. arrival with no breakage`;
