30dc8c1ff5d68a5951e9fa2d8af2415fbe961682
unknown <ziad.i.rida@gmail.com>
2017-08-23 07:31:23 +0300

    update

--- logger.js
+++ logger.js
@@ -1,15 +1,27 @@
 
 module.exports = class Logger {
-   constructor(logfile) {
+    logfile: string;
+    path: string;
+    logLevel: number;
+
+   constructor(logfile: string) {
      console.log("Logger: constructor");
        this.logfile = logfile;
    }
    display() {
        console.log("logFile:"+this.logfile );
    }
+   logLevel (level: number) {
+    this.loglevel = level;
+   }
    log(out1,out2,out3,out4,out5) {
      console.log("********* LogFile:",this.logfile);
      console.log((out1?out1:"")+(out2?out2:"")+(out3?out3:"")+(out4?out4:"")+(out5?out5:""))
 
    }
+
 }
+
+module.exports =  class SuperLogger extends Logger() {
+   
+ }
