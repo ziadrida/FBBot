0a43dca366f256ce8d39ec6f3d7620b1709653d7
unknown <ziad.i.rida@gmail.com>
2017-08-28 09:36:06 +0300

    update

--- index.js
+++ index.js
@@ -1061,10 +1061,14 @@ function sendTextMessage(recipientId, messageText,cb) {
       text: messageText
     }
   };
+  let timeout  = 3000
+  console.log("call callSendAPI **** - wait first for ",timeout)
+  setTimeout(function(){
+    console.log("now calling callSendAPI **** - after wait for ",timeout)
+    callSendAPI(messageData,function(){
+      if (cb) return callback();
+    })    ,timeout});
 
-  callSendAPI(messageData,function(){
-    if (cb) return callback();
-  });
 } // sendTextMessage
 
 
